c601598ad191927642a9090cc33810a4f31b78f2
Vyacheslav <kanto72@yandex.ru>
2021-03-25 14:13:04 +0500

    mda

--- js/script.js
+++ js/script.js
@@ -3,40 +3,10 @@ const swiper = new Swiper('.swiper-container', {
     spaceBetween: 28,
     slidesPerView: 3,
     // Navigation arrows
-    allowTouchMove: false,
+    // allowTouchMove: false,
 
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-    });
-
-    
-  
-    var galleryThumbs = new Swiper('.gallery-thumbs', {
-      spaceBetween: 10,
-      slidesPerView: 3,
-      freeMode: true,
-      // watchSlidesVisibility: true,
-      // watchSlidesProgress: true,
-      direction: 'vertical',
-      
-
-    });
-    var galleryTop = new Swiper('.gallery-top', {
-      spaceBetween: 10,
-      centeredSlides: true,
-      slidesPerView: 1,
-      keyboard: {
-        enabled: true,
-
-      },
-      allowTouchMove: false,
-      // navigation: {
-      //   nextEl: '.swiper-button-next',
-      //   prevEl: '.swiper-button-prev',
-      // },
-      thumbs: {
-        swiper: galleryThumbs
-      }
     });
