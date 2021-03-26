068fdf3f00beb4903785b51177e1167302f15435
Vyacheslav <kanto72@yandex.ru>
2021-03-26 16:42:00 +0500

    full-project adapt

--- js/script.js
+++ js/script.js
@@ -9,6 +9,27 @@ const swiper = new Swiper('.swiper-container', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      // when window width is >= 640px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 28
+      }
+    }
     });
 
 
